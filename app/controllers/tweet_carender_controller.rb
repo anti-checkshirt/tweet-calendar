4c58655fa7654fe0149c8e0b9ae713fd92488978
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-12-03 19:09:27 +0900

    Add Sample.jsonから欲しい情報のputs

--- app/controllers/tweet_carender_controller.rb
+++ app/controllers/tweet_carender_controller.rb
@@ -1,5 +1,17 @@
 class TweetCarenderController < ApplicationController
+
+  # ユーザー検索
   def show
     
+  end
+
+  # ユーザーのアップロードしたJSONファイルをDBに保存する
+  def index
+    json = ActiveSupport::JSON.decode(File.read('Sample.json'))
+    json.each do |data|
+      puts data['user']['screen_name']
+      puts data['text']
+      puts data['created_at']
+    end
   end
 end
