076db720e1f50a7f1fceb5ccdd20c52ad4ed2a61
s0ralit <s0ralit@protonmail.com>
2018-12-05 00:30:17 +0900

    ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root to: 'home#index'
 
-  get '/tweet_carender/:id', to: 'tweet_carender#show', as: 'tweet'
+  get '/search/:id', to: 'search#show'
+
+  # get '/tweet_carender/:id', to: 'tweet_carender#show', as: 'tweet'
 
 end
