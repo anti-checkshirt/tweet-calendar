66ccf48c3318c75dc4700b5fea5d84f849368203
s0ralit <s0ralit@protonmail.com>
2018-12-04 21:33:39 +0900

    [Add] .erb file これらを変更することによってデザインの変更が可能

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
 
-  root to: 'tweet_carender#index'
+  root to: 'home#index'
 
   get '/tweet_carender/:id', to: 'tweet_carender#show', as: 'tweet'
-  get '/tweet_carender' => 'tweet_carender#index'
 
 end
