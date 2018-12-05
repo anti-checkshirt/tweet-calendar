1548832843243b15b3bc8333141f29de7b75fd98
s0ralit <s0ralit@protonmail.com>
2018-12-05 19:22:15 +0900

    コントローラに条件文を追加 & erbファイルを修正

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,15 @@
 class SearchController < ApplicationController
   def index
     @tweet = Tweet.find_by(screen_name: params[:id])
-    p @tweet
+    # screen_nameを検索した際、街灯のユーザのツイートがない場合
+    if @tweet.nil?
+      p "Not found => tweet"
+    else
+      if params[:start_date].nil?
+        p "Not found => start_date"
+      else
+        p params[:start_date]
+      end
+    end
   end
 end
