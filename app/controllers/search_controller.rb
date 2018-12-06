eb88c29ef56dcd16d2b2a09cbc767b46d51e452f
s0ralit <s0ralit@protonmail.com>
2018-12-07 01:19:45 +0900

    dbから欲しいデータを取れるようにした

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,22 @@
+require "date"
 class SearchController < ApplicationController
   def index
-    @tweet = Tweet.find_by(screen_name: params[:id])
+
     # screen_nameを検索した際、該当のユーザのツイートがない場合
-    if @tweet.nil?
-      p "Not found => tweet"
+    if !Tweet.exists?(screen_name: params[:id])
+      p "Not found => screen_name"
     else
+      @tweets = Tweet.where(screen_name: params[:id])
       if params[:start_date].nil?
-        p "Not found => start_date"
+        today_date = Date.today
+        start_date = today_date.year.to_s + "-" + today_date.month.to_s
+        @tweets.each do |tweet|
+          if tweet[:tweet_created_at].include?(start_date.to_s)
+            p tweet
+          else
+            p "Not found => tweet"
+          end
+        end
       else
         p params[:start_date]
       end
