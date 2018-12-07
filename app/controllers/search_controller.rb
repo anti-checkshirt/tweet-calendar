85ec2124b36eaa70ac3cae690826f90ce6b74962
s0ralit <s0ralit@protonmail.com>
2018-12-07 18:26:49 +0900

    ツイートを時系列順に取得できるようにした

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,18 +6,20 @@ class SearchController < ApplicationController
     if !Tweet.exists?(screen_name: params[:id])
       p "Not found => screen_name"
     else
-      @tweets = Tweet.where(screen_name: params[:id])
-      if params[:start_date].nil?
+      @tweets = Tweet.where(screen_name: params[:id]).order(tweet_created_at: :desc)
+      # p @tweets      
+      if params[:start_date] != nil
         today_date = Date.today
         start_date = today_date.year.to_s + "-" + today_date.month.to_s
         @tweets.each do |tweet|
-          if tweet[:tweet_created_at].include?(start_date.to_s)
+          if tweet[:tweet_created_at].include?(params[:start_date].to_s)
             p tweet
           else
-            p "Not found => tweet"
+            return "Not found => tweet"
           end
         end
       else
+        # ここに実装をどうするか悩んでます。
         p params[:start_date]
       end
     end
