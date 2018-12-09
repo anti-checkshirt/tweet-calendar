472912a4778c55ead551c38df06d0a50d838ab5d
s0ralit <s0ralit@protonmail.com>
2018-12-10 01:03:40 +0900

    指定した情報を取得できるようにしたはず

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -1,8 +1,17 @@
 class Api::V1::SearchController < ApplicationController
   def index
-    @tweet = Tweet.where(screen_name: params[:id])
-    
-    render json: @tweet
+    render_json = {}
+    @tweets = Tweet.where(screen_name: params[:id])
+    @tweets.each do |tweet|
+      if tweet[:tweet_created_at].include?(params[:date])
+        p tweet
+        render_json["screen_name"] = tweet.screen_name
+        render_json["text"] = tweet.text
+      else
+        render_json["Error"] = "Not found."
+      end
+    end
+    render json: render_json
   end
 
   private
