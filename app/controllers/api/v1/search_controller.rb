4195496c6e7832cd235647abd9a0f51021c5bf14
s0ralit <s0ralit@protonmail.com>
2018-12-10 00:56:52 +0900

    とりあえずid検索はできるようにした

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::SearchController < ApplicationController
   def index
-    @tweet = Tweet.where(screen_name: params[:id], tweet_created_at: params[:date])
+    @tweet = Tweet.where(screen_name: params[:id])
     
     render json: @tweet
   end
