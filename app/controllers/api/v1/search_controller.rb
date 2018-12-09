95c466d1dfb8b977440ea48a757415518d904966
s0ralit <s0ralit@protonmail.com>
2018-12-10 01:20:51 +0900

    見た目はいい感じ、中身はダメ

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -1,14 +1,10 @@
 class Api::V1::SearchController < ApplicationController
   def index
-    tweet_json = {}
+    tweet_json = []
     @tweets = Tweet.where(screen_name: params[:id])
     @tweets.each do |tweet|
       if tweet[:tweet_created_at].include?(params[:date])
-        p tweet
-        tweet_json["screen_name"] = tweet.screen_name
-        tweet_json["text"] = tweet.text
-      else
-        tweet_json["Error"] = "Not found."
+        tweet_json.push(tweet)
       end
     end
     render json: tweet_json
