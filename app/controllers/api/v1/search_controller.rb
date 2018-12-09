5d0bb1dc5b03bfe834d3c6c6a600909b84c98070
s0ralit <s0ralit@protonmail.com>
2018-12-10 01:33:58 +0900

    見た目はいい感じ、中身はダメです

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::SearchController < ApplicationController
   def index
+    # 他に絶対いい方法があるはずなのでコード修正していくと思います。
     tweet_json = []
     @tweets = Tweet.where(screen_name: params[:id])
     @tweets.each do |tweet|
@@ -7,7 +8,11 @@ class Api::V1::SearchController < ApplicationController
         tweet_json.push(tweet)
       end
     end
-    render json: tweet_json
+    if tweet_json.empty?
+      render json: '{"404":"Not Found"}'
+    else
+      render json: tweet_json
+    end
   end
 
   private
